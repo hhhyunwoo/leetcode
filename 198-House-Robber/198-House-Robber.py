d2524d1102a1d36da68bc28fcc1f61c11f43aab6
hhhyunwoo <qgusdngusdn@gmail.com>
2023-05-13 18:25:13 +0900

    Update 198-House-Robber/198-House-Robber.py DP

--- 198-House-Robber/198-House-Robber.py
+++ 198-House-Robber/198-House-Robber.py
@@ -4,10 +4,10 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         if len(nums) <= 2:
             return max(nums)
-        dp = {0: nums[0], 1: nums[1]}
-        dp[2] = max(dp[0] + nums[2], dp[1])
-        for i in range(3, len(nums)):
-            dp[i] = max(dp[i-3] + nums[i], dp[i-2] + nums[i], dp[i-1])
+        dp = {0: nums[0], 1: max(nums[0], nums[1])}
+
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-2] + nums[i], dp[i-1])
 
         return max(list(dp.values()))
 
