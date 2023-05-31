1a069cc98215b0036624ab81b6173d8334f44370
hhhyunwoo <qgusdngusdn@gmail.com>
2023-05-31 10:50:59 +0900

    Update

--- 141-Linked-List-Cycle/141-Linked-List-Cycle.py
+++ 141-Linked-List-Cycle/141-Linked-List-Cycle.py
@@ -1,18 +1,19 @@
-class ListNode:
-     def __init__(self, x):
-         self.val = x
-         self.next = None
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        if head is None:
+        if head is None or head.next is None:
             return False
         tortoise, hare = head, head
         while True:
-            if head.next is None or head.next.next is None:
+            if hare is None or hare.next is None:
                 return False
-            tortoise = head.next
-            hare = head.next.next
+            tortoise = tortoise.next
+            hare = hare.next.next
 
             if tortoise == hare:
                 return True
