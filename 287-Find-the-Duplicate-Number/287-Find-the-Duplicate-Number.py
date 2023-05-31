ccda121779b097e73cbe4a5a470af41f968a497d
hhhyunwoo <qgusdngusdn@gmail.com>
2023-05-31 14:13:41 +0900

    Add new solve

--- 287-Find-the-Duplicate-Number/287-Find-the-Duplicate-Number.py
+++ 287-Find-the-Duplicate-Number/287-Find-the-Duplicate-Number.py
@@ -1,3 +1,28 @@
+from typing import List
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        tortoise, hare = 0,0
+
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[nums[hare]]
+            if tortoise == hare:
+                break
+                
+        tortoise = 0
+        while True:
+            tortoise = nums[tortoise]
+            hare = nums[hare]
+            if tortoise == hare:
+                return tortoise
+
+sol = Solution()
+print(sol.findDuplicate([1,3,4,2,2]))        
+
+
+
+###############
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
         numDict = {}
@@ -6,4 +31,3 @@ class Solution:
                 return num
             else:
                 numDict[num] = 1
-        
