6cbea5cf1654474ed9a923096a1943d278d7ae0f
hhhyunwoo <qgusdngusdn@gmail.com>
2023-06-03 19:43:57 +0900

    Solve 451-Sort-Characters-By-Frequency/

---
+++ 451-Sort-Characters-By-Frequency/451-Sort-Characters-By-Frequency.py
@@ -0,0 +1,19 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        sDict = {s[i]: 0 for i in range(len(s))}
+        for i in range(len(s)):
+            sDict[s[i]] += 1
+
+
+        sSortedArr = [k for k,v in sorted(sDict.items(), key=lambda item: item[1], reverse=True)]
+
+
+        answer = []
+        for c in sSortedArr:
+            print(c) 
+            for _ in range(sDict[c]):
+                answer.append(c)
+        return answer
+
+sol = Solution()
+print(sol.frequencySort("Aabb"))
