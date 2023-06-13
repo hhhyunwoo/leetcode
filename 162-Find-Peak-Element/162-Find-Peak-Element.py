b25fddbf1956b3c242a4a3c2a4fdee5120088119
hhhyunwoo <qgusdngusdn@gmail.com>
2023-06-13 13:30:35 +0900

    Solve 162-Find-Peak-Element/

---
+++ 162-Find-Peak-Element/162-Find-Peak-Element.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        l,r = 0, len(nums) - 1
+
+        while l < r:
+            mid = l + (r - l) // 2
+            if nums[mid] < nums[mid+1]:
+                l = mid + 1
+            else:
+                r = mid
+        return l
+
+
+sol = Solution()
+print(sol.findPeakElement([1,2]))
