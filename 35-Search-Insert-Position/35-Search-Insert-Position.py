717c71c4cdfb0cd0b8d154423040494eb197815a
hhhyunwoo <qgusdngusdn@gmail.com>
2023-08-28 23:34:20 +0900

    Update 35

--- 35-Search-Insert-Position/35-Search-Insert-Position.py
+++ 35-Search-Insert-Position/35-Search-Insert-Position.py
@@ -1,4 +1,3 @@
-from typing import List
 
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
@@ -14,6 +13,5 @@ class Solution:
                 r = mid - 1
             else:
                 l = mid + 1
-        return l + 1
-
-print(Solution().searchInsert([1,3,5,6],7))
+        if nums[l] < target: return l + 1
+        else: return l
